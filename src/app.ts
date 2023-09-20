4705963b82f941701f80bfac58be64fa29ef76be
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-09-20 17:00:10 -0500

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
